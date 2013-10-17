b020ec0734dbd68104a718248b20c0512c52f5e6
Jonah <jjmgruber@gmail.com>
2013-10-17 17:29:47 -0400

    Added a function for drawing a circle with html5 canvas and a function or a random hex color

--- assignment2.js
+++ assignment2.js
@@ -1,6 +1,30 @@
 $.fn.hexed = function(settings){
 
 
+	function createCanvas(color){
+		var canvas = $("<canvas></canvas>");
+		canvas.attr("id", "canvas");
+		canvas.attr("heigh", 200);
+		canvas.attr("width", 200);
+		var context = canvas[0].getContext("2d");
+		// Set the color of the fill
+		context.fillStyle="#" + color;
+
+		// Start a new path
+		context.beginPath();
+
+		context.arc(100,100,50,0,Math.PI*2,true);
+
+		// Close the path
+		context.closePath();
+
+		// Fills in the arc. Since we set the fillStyle to have color red, it fill it red.
+		context.fill();
+		context.stroke();
+
+		return canvas;
+	}
+
 
 	function createSlider(name){
 		var slider = $("<input>");
@@ -49,32 +73,43 @@ $.fn.hexed = function(settings){
 		return button;
 	}
 
-	function createTimer(){
-		var text = $("<p></p>");		
-		var time = 0;
-		text.attr("id", "timer");
-		text.html("0 seconds");
-		return this;
-	}
-	function getTime(timer){
-		return timer.time;
+	function getRandomColor(){
+		return Math.floor(Math.random()*16777215).toString(16);
 	}
 
-	function update(timer){
-		timer.time = timer.time + 1;
-		$("#timer").html(timer.time + "seconds");
-
+	function scoreGame(color, name1, name2, name3){
+		
 	}
 
+	// function createTimer(){
+	// 	var text = $("<p></p>");		
+	// 	var time = 0;
+	// 	text.attr("id", "timer");
+	// 	text.html("0 seconds");
+	// 	return this;
+	// }
+	// function getTime(timer){
+	// 	return timer.time;
+	// }
+
+	// function update(timer){
+	// 	timer.time = timer.time + 1;
+	// 	$("#timer").html(timer.time + "seconds");
+
+	// }
+	var color = getRandomColor();
+	this.html("");
+	this.append(createCanvas(color));
 	this.append(createSlider("Red"));
 	this.append(createSlider("Green"));
 	this.append(createSlider("Blue"));
 	this.append(createSubmitButton());
+	scoreGame(color, "Red", "Green", "Blue");
 	
-	var timer = createTimer();
-	this.append(timer.text);
-	while(getTime(timer) < 30){
-		$.delay(1000).update(timer);
-	}
+	// var timer = createTimer();
+	// this.append(timer.text);
+	// while(getTime(timer) < 30){
+	// 	$.delay(1000).update(timer);
+	// }
 };
 
